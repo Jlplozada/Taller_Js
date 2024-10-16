1fcc36d78406a45aa736083a408a1695df0957d3
jlplozada <jose_lpaezl@soy.sena.edu.co>
2024-10-15 21:20:43 -0500

    clase

---
+++ prototipo/fecha.js
@@ -0,0 +1,26 @@
+let fecha = new Date();
+
+console.log(fecha);
+
+console.log(fecha.getDate());
+
+console.log(fecha.getDay());
+
+console.log(fecha.getMonth());
+
+console.log(fecha.getFullYear());
+
+console.log(fecha.getHours());
+
+console.log(fecha.getMinutes());
+
+console.log(fecha.toString());
+
+console.log(fecha.toDateString());
+
+console.log(fecha.getTimezoneOffset());
+
+let nacimiento = new Date("2000,12,05");
+console.log(nacimiento);
+
+
---
+++ prototipo/modulo.js
@@ -0,0 +1,3 @@
+import { aritmetica } from "./matematicas.js";
+// console.log(aritmetica.suma(5,4));
+console.log(aritmetica(5,4));
